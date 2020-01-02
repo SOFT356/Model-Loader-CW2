191636eb086fb853813474e323ac2dfc528f6e72
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2020-01-02 16:07:35 +0000

    Added camera

--- Project5/ShadedCube.cpp
+++ Project5/ShadedCube.cpp
@@ -20,6 +20,8 @@
 #include <vector>
 #include <algorithm>
 
+//#include <Camera.h>
+
 using namespace std;
 
 // to use this example you will need to download the header files for GLM put them into a folder which you will reference in
@@ -35,6 +37,26 @@ GLuint texture1;
 GLuint shader;
 
 GLuint NumVertices = 36;
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 //----------------------------------------------------------------------------
 //
@@ -370,179 +392,7 @@ init(vector<glm::vec3>& vertices, vector<glm::vec2>& uvs, vector<glm::vec3>& nor
 	glUniform1fv(sShineLoc, 1, &shininess);
 
 
-	// setting up the cube
-
-	//GLfloat vertices[] = {
-	//-0.5f, -0.5f, -0.5f, //0 b l
-	// 0.5f, -0.5f, -0.5f, //1 b r
-	// 0.5f,  0.5f, -0.5f, //2 t r
-	// 0.5f,  0.5f, -0.5f, //3 t r
-	//-0.5f,  0.5f, -0.5f, //4 t l 
-	//-0.5f, -0.5f, -0.5f, //5 b l
-
-	//-0.5f, -0.5f,  0.5f, //6 b l
-	// 0.5f, -0.5f,  0.5f, //7 b r
-	// 0.5f,  0.5f,  0.5f, //8 t r
-	// 0.5f,  0.5f,  0.5f, //9 t r 
-	//-0.5f,  0.5f,  0.5f, //10 t l
-	//-0.5f, -0.5f,  0.5f, //11 b l
-
-	//-0.5f,  0.5f,  0.5f,
-	//-0.5f,  0.5f, -0.5f,
-	//-0.5f, -0.5f, -0.5f,
-	//-0.5f, -0.5f, -0.5f,
-	//-0.5f, -0.5f,  0.5f,
-	//-0.5f,  0.5f,  0.5f,
-
-	// 0.5f,  0.5f,  0.5f,
-	// 0.5f,  0.5f, -0.5f,
-	// 0.5f, -0.5f, -0.5f,
-	// 0.5f, -0.5f, -0.5f,
-	// 0.5f, -0.5f,  0.5f,
-	// 0.5f,  0.5f,  0.5f,
-
-	//-0.5f, -0.5f, -0.5f, // b l b
-	// 0.5f, -0.5f, -0.5f, // b r b
-	// 0.5f, -0.5f,  0.5f, // b r f
-	// 0.5f, -0.5f,  0.5f, // b r b
-	//-0.5f, -0.5f,  0.5f, // b l f
-	//-0.5f, -0.5f, -0.5f, // b l b
-
-	//-0.5f,  0.5f, -0.5f,
-	// 0.5f,  0.5f, -0.5f,
-	// 0.5f,  0.5f,  0.5f,
-	// 0.5f,  0.5f,  0.5f,
-	//-0.5f,  0.5f,  0.5f,
-	//-0.5f,  0.5f, -0.5f,
-	//};
-
-	//GLfloat normals[]{
-	//0.0f,  0.0f, -1.0f,
-	//0.0f,  0.0f, -1.0f,
-	//0.0f,  0.0f, -1.0f,
-	//0.0f,  0.0f, -1.0f,
-	//0.0f,  0.0f, -1.0f,
-	//0.0f,  0.0f, -1.0f,
-
-	//0.0f,  0.0f, 1.0f,
-	//0.0f,  0.0f, 1.0f,
-	//0.0f,  0.0f, 1.0f,
-	//0.0f,  0.0f, 1.0f,
-	//0.0f,  0.0f, 1.0f,
-	//0.0f,  0.0f, 1.0f,
-
-	//-1.0f,  0.0f,  0.0f,
-	//-1.0f,  0.0f,  0.0f,
-	//-1.0f,  0.0f,  0.0f,
-	//-1.0f,  0.0f,  0.0f,
-	//-1.0f,  0.0f,  0.0f,
-	//-1.0f,  0.0f,  0.0f,
-	//
-	//1.0f,  0.0f,  0.0f,
-	//1.0f,  0.0f,  0.0f,
-	//1.0f,  0.0f,  0.0f,
-	//1.0f,  0.0f,  0.0f,
-	//1.0f,  0.0f,  0.0f,
-	//1.0f,  0.0f,  0.0f,
-
-	//0.0f, -1.0f,  0.0f,
-	//0.0f, -1.0f,  0.0f,
-	//0.0f, -1.0f,  0.0f,
-	//0.0f, -1.0f,  0.0f,
-	//0.0f, -1.0f,  0.0f,
-	//0.0f, -1.0f,  0.0f,
-
-	//0.0f,  1.0f,  0.0f,
-	//0.0f,  1.0f,  0.0f,
-	//0.0f,  1.0f,  0.0f,
-	//0.0f,  1.0f,  0.0f,
-	//0.0f,  1.0f,  0.0f,
-	//0.0f,  1.0f,  0.0f};
-
-	GLuint indices[][3] = {  // note that we start from 0!
-		{0, 1, 2},  // first Triangle front
-		{3, 4, 5},   // second Triangle
-		
-		{8, 7, 6 },
-		{11, 10, 9 },
-		
-		{14, 13, 12 },
-		{17, 16, 15 },
-		
-		{18, 19, 20 },
-		{21, 22, 23 },
-		
-		{26, 25, 24 },
-		{29, 28, 27 },
-		
-		{30, 31, 32 },  // first Triangle back
-		{33, 34, 35 }   // second Triangle
-	};
-
-	/*GLfloat  colours[][4] = {
-		{ 1.0f, 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f },
-	    { 1.0f, 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f, 1.0f },
-	    
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f, 1.0f },
-		{ 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f, 1.0f },
-		
-		{ 0.0f, 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f, 1.0f },
-		{ 0.0f, 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 1.0f, 1.0f },
-		
-		{ 1.0f, 0.0f, 1.0f, 1.0f }, { 1.0f, 0.0f, 1.0f, 1.0f }, { 1.0f, 0.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.0f, 1.0f, 1.0f }, { 1.0f, 0.0f, 1.0f, 1.0f }, { 1.0f, 0.0f, 1.0f, 1.0f },
-		
-		{ 1.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f, 0.0f, 1.0f },
-		{ 1.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f, 0.0f, 1.0f }, { 1.0f, 1.0f, 0.0f, 1.0f },
-		
-		{ 0.0f, 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f, 1.0f, 1.0f },
-		{ 0.0f, 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f, 1.0f, 1.0f },
-		
-	};*/
-	/*GLfloat  texture_coords[] = {
-		0.0f, 0.0f,
-		1.0f,0.0f,
-		1.0f, 1.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f,0.0f,
-		1.0f, 1.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f,0.0f,
-		1.0f, 1.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f,0.0f,
-		1.0f, 1.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f,0.0f,
-		1.0f, 1.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f,
-
-		0.0f, 0.0f,
-		1.0f,0.0f,
-		1.0f, 1.0f,
-		1.0f, 1.0f,
-		0.0f, 1.0f,
-		0.0f, 0.0f
-	};*/
-
+	
 	 NumVertices = vertices.size();
 	vector<glm::vec4> color;
 	for (size_t i = 0; i < NumVertices; i++)
@@ -582,7 +432,8 @@ init(vector<glm::vec3>& vertices, vector<glm::vec2>& uvs, vector<glm::vec3>& nor
 
 	//Texture Binding
 	glBindBuffer(GL_ARRAY_BUFFER, Buffers[Textures]);
-	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);	glVertexAttribPointer(Textures, 2, GL_FLOAT,
+	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);	
+	glVertexAttribPointer(Textures, 2, GL_FLOAT,
 		GL_FALSE, 0, BUFFER_OFFSET(0));
 
 	
@@ -640,6 +491,21 @@ display(GLfloat delta)
 	glClearBufferfv(GL_COLOR, 0, black);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+
+	/*glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+	glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);*/
+
+	
+
+
 	// bind textures on corresponding texture units
 	//glFrontFace(GL_CW);
 	//glCullFace(GL_BACK);
@@ -651,11 +517,10 @@ display(GLfloat delta)
 
 
 	// creating the view matrix
-	glm::mat4 view = glm::mat4(1.0f);
-	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
+	glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
-	// creating the projection matrix
-	glm::mat4 projection = glm::perspective(45.0f, 4.0f / 3, 0.1f, 20.0f);
+	// pass projection matrix to shader (note that in this case it could change every frame)
+	glm::mat4 projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
 	// Adding all matrices up to create combined matrix
 	glm::mat4 mv = view * model;
@@ -676,6 +541,80 @@ display(GLfloat delta)
 	//glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 }
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	float cameraSpeed = 2.5 * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		cameraPos += cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		cameraPos -= cameraSpeed * cameraFront;
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (fov >= 1.0f && fov <= 45.0f)
+		fov -= yoffset;
+	if (fov <= 1.0f)
+		fov = 1.0f;
+	if (fov >= 45.0f)
+		fov = 45.0f;
+}
+
 
 //----------------------------------------------------------------------------
 //
@@ -708,12 +647,23 @@ main(int argc, char** argv)
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Shaded Cube", NULL, NULL);
 
 	glfwMakeContextCurrent(window);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	glewInit();
 
 	init(vertices, uvs, normals, colour, diffuse, specular, specularExponent, textureName);
 	GLfloat timer= 0.0f;
 	while (!glfwWindowShouldClose(window))
 	{
+
+		processInput(window);
+
 		// uncomment to draw only wireframe 
 		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
